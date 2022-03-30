10dd61629cddbfe0212300fb1d5294fd93b41d58
MatiaPercic <mapercic@unipu.hr>
2022-03-30 10:55:33 +0200

    created GetCommit in components

---
+++ src/components/GetCommit.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <ul id="example-1">
+        <li v-for="commit in this.commits" :key="commit">
+            Commit: <router-link to="/details/sha" @click="setCommit(commit.sha)">{{ commit.sha }}</router-link>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'GetCommit',
+  props: {
+    url: URL
+  },
+  data() {
+        return { commits: [] }
+    },
+        methods: {
+            async getData() {
+            try {
+                const response = await axios.get(
+                "https://api.github.com/repos/vuejs/vue/commits"
+                );
+                this.commits = response.data;
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+            parse() {
+                console.log(commits);
+            },
+            setCommit(sha) {
+                localStorage.setItem("wantedCommit", sha)
+            }
+        },
+        created() {
+            this.getData();
+        }
+}
+</script>
